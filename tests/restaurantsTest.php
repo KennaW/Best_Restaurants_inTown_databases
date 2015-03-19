046b56ff9b914299749fba63f91da111b41ba70c
tommy bountham & kenna warsinske <student@epicodus.com>
2015-03-19 16:34:18 -0700

    end of day

--- tests/restaurantsTest.php
+++ tests/restaurantsTest.php
@@ -6,6 +6,7 @@
     */
 
     require_once "src/restaurant.php";
+    require_once "src/cuisine.php";
 
     $DB = new PDO('pgsql:host=localhost;dbname=test_best_restaurants');
 
@@ -19,13 +20,42 @@
         function test_getId()
         {
             //Arrange
-            $name = "Home Town Buffet";
+            $cuisine = "Soul Food";
             $id = null;
-            $test_name = new Restaurant($name, $id);
-            $test_name->save();
+            $test_cuisine = new Cuisine($cuisine, $id);
+            $test_cuisine->save();
+
+            $restaurant_name = "Home Town Buffet";
+            $cuisine_id = $test_cuisine->getId();
+            $test_restaurant = new Restaurant($restaurant_name, $id, $cuisine_id);
+            $test_restaurant->save();
+
 
             //Act
-            $result = $test_name->getId();
+            $result = $test_restaurant->getId();
+
+            //Assert
+            $this->assertEquals(true, is_numeric($result));
+        }
+
+
+        function test_cuisineId()
+        {
+            //Arrang
+            $cuisine = "Soul food";
+            $id = null;
+            $test_cuisine = new Cuisine($cuisine, $id);
+            $test_cuisine->save();
+
+            $restaurant_name = "Home Town Buffet";
+            $cuisine_id = $test_cuisine->getId();
+            $test_restaurant = new Restaurant($restaurant_name, $id, $cuisine_id);
+            $test_restaurant->save();
+
+
+            //Act
+            $result = $test_restaurant->getCuisineId();
+
 
             //Assert
             $this->assertEquals(true, is_numeric($result));
